933418ed8f8b401a2e1dab9289dccd1fdd7b0f67
yhdavidwu <140509648+yhdavidwu@users.noreply.github.com>
2023-09-05 12:33:06 +0000

    modify

---
+++ lesson13_2.py
@@ -0,0 +1,9 @@
+import student
+
+stu1 = student.Student("徐小明",78,92,68)
+print(f'中文:{stu1.chinese}')
+print(f'英文:{stu1.english}')
+print(f'數學:{stu1.math}')
+print(stu1.total())
+print(stu1.average)
+print(stu1)
